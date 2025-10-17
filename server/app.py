ca924f11939742ce83c97a0ea7c01dcce0f1de44
Dev Gupta <devg629@gmail.com>
2025-10-17 23:44:53 +0530

    'app.py from server.py '

--- server/app.py
+++ server/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import util
-app =  Flask(__name__)
+import os
 
+app = Flask(__name__)
 
+# Route to get all location names
 @app.route('/get_location_names')
 def get_location_names():
     response = jsonify({
@@ -11,20 +13,26 @@ def get_location_names():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-
-@app.route('/predict_house_price', methods=['post'])
+# Route to predict house price
+@app.route('/predict_house_price', methods=['POST'])
 def predict_house_price():
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
     bhk = int(request.form['bhk'])
     bath = int(request.form['bath'])
 
+    estimated_price = util.get_estimated_price(location, total_sqft, bhk, bath)
+
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft, bhk, bath)
+        'estimated_price': estimated_price
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
 if __name__ == '__main__':
-    print('Starting Python server for Bengaluru House Prediction Project')
+    print('Starting Python server for Bengaluru House Price Prediction')
     util.load_saved_artifacts()
-    app.run()
+    
+    # Use environment PORT if deployed on cloud (Render/Heroku)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
--- server/util.py
+++ server/util.py
@@ -1,41 +1,56 @@
 import json
 import pickle
 import numpy as np
-__locations =None
+
+# Global variables to hold model and metadata
+__locations = None
 __data_columns = None
 __model = None
 
-
-def get_estimated_price(location,sqft,bhk,bath):
+# Function to estimate price
+def get_estimated_price(location, sqft, bhk, bath):
     try:
-        loc_index =  __data_columns.index(location.lower())
-    except:
+        loc_index = __data_columns.index(location.lower())
+    except ValueError:
         loc_index = -1
+
+    # Prepare input array
     x = np.zeros(len(__data_columns))
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
+
     if loc_index >= 0:
         x[loc_index] = 1
 
-    return round(__model.predict([x])[0],2)
+    return round(__model.predict([x])[0], 2)
 
+# Function to get all locations
 def get_location_names():
-    return __locations
+    if __locations:
+        return [loc.title() for loc in __locations]  # Capitalized names for front-end
+    return []
 
+# Function to load model and columns
 def load_saved_artifacts():
     print('Loading saved artifacts...')
-    global __locations
     global __data_columns
+    global __locations
+    global __model
+
+    # Load column info
     with open('./artifacts/columns.json', 'r') as f:
         __data_columns = json.load(f)['data_columns']
-        __locations = __data_columns[3:]
-    global __model
+        __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
+
+    # Load trained model
     with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f:
         __model = pickle.load(f)
 
+    print('Artifacts loaded successfully!')
 
+# For testing
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
-    # print(get_estimated_price('indranagar',1500,3,4))
+    # print(get_estimated_price('Indiranagar', 1500, 3, 3))
